2af1c37d0b322dc9f36470217154b79c3772a454
Jatin Aneja <jatin.aneja@tekgem.co.uk>
2022-07-10 10:07:20 +0100

    Added skills

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -8,21 +8,21 @@ export default function Skills(props) {
         <h2 class="skill-header">My Top Skills</h2>
 
         <div class="skills-wrapper">
-          <div class="first-set animate__animated animate__pulse">
+          <div  class="first-set animate__animated animate__pulse">
             <img
-              src="../img/skills/JavaScriptIcon.png"
+              src="https://th.bing.com/th/id/OIP.8AcIyzgCiPl6P7-nKGLOGwHaHa?pid=ImgDet&rs=1"
               alt=""
               loading="lazy"
               class="icon icon-card"
             />
              <img
-              src="../img/skills/CSSIcon.png"
+              src="https://th.bing.com/th/id/R.31cf6505344555f2e76cb68e832a39d7?rik=LsUBn1btw79yjA&pid=ImgRaw&r=0"
               alt=""
               loading="lazy"
               class="icon icon-card"
             />
              <img
-              src="../img/skills/HTMLIcon.png"
+              src="https://th.bing.com/th/id/R.6044cf20dcfdad6a1bea96fdd4dbe822?rik=kS50g65y0O0IRA&pid=ImgRaw&r=0"
               alt=""
               loading="lazy"
               class="icon icon-card"
@@ -34,19 +34,19 @@ export default function Skills(props) {
               class="icon icon-card"
             />
              <img
-              src="../img/skills/CppIcon.png"
+              src="https://e7.pngegg.com/pngimages/724/306/png-clipart-c-logo-c-programming-language-icon-letter-c-blue-logo.png"
               alt=""
               loading="lazy"
               class="icon icon-card"
             />
              <img
-              src="../img/skills/javaicon.png"
+              src="https://th.bing.com/th/id/OIP.VZYJGn2VK2FBE0t2tLvvrwHaHa?pid=ImgDet&rs=1"
               alt=""
               loading="lazy"
               class="icon icon-card"
             />
              <img
-              src="../img/skills/AndroidIcon.png"
+              src="https://th.bing.com/th/id/R.e641f40aa128878e4d9039eac0a946d1?rik=EvA4XYF%2bJEKF3A&riu=http%3a%2f%2ffc01.deviantart.net%2ffs70%2fi%2f2011%2f257%2f5%2f2%2fandroid_svg_by_phlum-d49uf7z.png&ehk=J5YdYtYSDF84GYkkIHqlff0f51nX0YejT65Gl6yX8DY%3d&risl=&pid=ImgRaw&r=0"
               alt=""
               loading="lazy"
               class="icon icon-card"
@@ -54,7 +54,7 @@ export default function Skills(props) {
           </div>
           <div class="second-set animate__animated animate__pulse">
           <img
-              src="../img/skills/PythonIcon.png"
+              src="https://th.bing.com/th/id/R.bd3021ca52d5bc6d63c5fa1b3c722d58?rik=g%2bctcXXPqKswyA&pid=ImgRaw&r=0"
               alt=""
               loading="lazy"
               class="icon icon-card"
@@ -66,25 +66,25 @@ export default function Skills(props) {
               class="icon icon-card"
             />
              <img
-              src="../img/skills/ReactjsIcon.png"
+              src="https://th.bing.com/th/id/R.8cce6617d14fa0d818a54646f295909d?rik=ly1aNhwl4Jaj%2bw&pid=ImgRaw&r=0&sres=1&sresct=1"
               alt=""
               loading="lazy"
               class="icon icon-card"
             />
              <img
-              src="../img/skills/NodejsIcon.png"
+              src="https://th.bing.com/th/id/R.6217b547de02228b6bdac7bb59fe9341?rik=hOzWi0J%2bR1qgrw&pid=ImgRaw&r=0&sres=1&sresct=1"
               alt=""
               loading="lazy"
               class="icon icon-card"
             />
              <img
-              src="../img/skills/MySQLIcon.png"
+              src="https://th.bing.com/th/id/OIP.lIIc_svaWdGdEJuEk7TBlgHaHa?pid=ImgDet&rs=1"
               alt=""
               loading="lazy"
               class="icon icon-card"
             />
              <img
-              src="../img/skills/MongoDBIcon.png"
+              src="https://pluspng.com/img-png/mongodb-png--770.png"
               alt=""
               loading="lazy"
               class="icon icon-card"
@@ -93,37 +93,43 @@ export default function Skills(props) {
           </div>
           <div class="second-set animate__animated animate__pulse">
           <img
-              src="../img/skills/FirebaseIcon.png"
+              src="https://pluspng.com/img-png/firebase-logo-png-firebase-logo-png-transparent-amp-svg-vector-pluspng-2400x3291.png"
               alt=""
               loading="lazy"
               class="icon icon-card"
             />
              <img
-              src="../img/skills/JSXIcon.png"
+              src="https://th.bing.com/th/id/R.ca7ce6e1ad578a04d7fedc81c5c8b32d?rik=aVv6HeeiKvo9rQ&pid=ImgRaw&r=0"
               alt=""
               loading="lazy"
               class="icon icon-card"
             />
              <img
-              src="../img/skills/CloudAPIIcon.png"
+              src="https://th.bing.com/th/id/R.07ca2f32240ddfc91b7aefb8c8ad3e54?rik=Ld2Ci64o8Om%2fdQ&riu=http%3a%2f%2fpluspng.com%2fimg-png%2fgoogle-cloud-logo-png-library-of-google-cloud-logo-picture-royalty-free-stock-png-files-960x960.png&ehk=4RjUCmZ8lvjDgzmDvQeGEegpnzEde7PRl%2b0RlhJqa6E%3d&risl=&pid=ImgRaw&r=0"
               alt=""
               loading="lazy"
               class="icon icon-card"
             />
              <img
-              src="../img/skills/GithubIcon.png"
+              src="https://library.kissclipart.com/20181213/tce/kissclipart-octopus-github-clipart-github-version-control-debf10e27dfc1ccd.png"
               alt=""
               loading="lazy"
               class="icon icon-card"
             />
              <img
-              src="../img/skills/GitIcon.png"
+              src="https://th.bing.com/th/id/R.df16ad2202f21e78bf5a2ea61393bb92?rik=lfTSDKWQ52A%2b%2bw&pid=ImgRaw&r=0"
               alt=""
               loading="lazy"
               class="icon icon-card"
             />
              <img
-              src="../img/skills/SystemDesignIcon.png"
+              src="https://th.bing.com/th/id/R.84bb0ce1e965ff05ccd81810d1b05d2d?rik=oR2QVNMU7pkb0w&riu=http%3a%2f%2fwww.pngmart.com%2ffiles%2f3%2fSoftware-PNG-Picture.png&ehk=lWIt6ELBXztL%2ftqpnpMx4olA2y1EFY7UEy83sVd6cz0%3d&risl=&pid=ImgRaw&r=0"
+              alt=""
+              loading="lazy"
+              class="icon icon-card"
+            />
+             <img
+              src="https://th.bing.com/th/id/OIP.TSYkzzNU-MdIIlEnxuZpGQHaHa?pid=ImgDet&rs=1"
               alt=""
               loading="lazy"
               class="icon icon-card"
